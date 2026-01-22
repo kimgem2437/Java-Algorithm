967fcda47679decf98e79e01079860ec5d7005be
KimGemini <rlaalswo2437@gmail.com>
2026-01-23 00:16:48 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,14 @@
+public class Solution {
+	public int solution(int n) {
+		
+		String N = String.valueOf(n);
+		
+		int sum = 0;
+		
+		for(int i = 0; i < N.length(); i++) {
+			sum += N.charAt(i) - '0';
+		}
+		
+		return sum;
+	}
+}
