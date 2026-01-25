c8ebe960b661c59d8636a501fdbbbc3e7a17a3f4
KimGemini <rlaalswo2437@gmail.com>
2026-01-25 23:27:21 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.02 ms, Memory: 87.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.java
@@ -0,0 +1,16 @@
+
+public class Solution {
+	public String solution(int num) {
+		String answer = "";
+		
+		if(num % 2 != 0) {
+			answer = "Odd";
+		} else {
+			answer = "Even";
+		}
+		
+		
+		return answer;
+	}
+	
+}
