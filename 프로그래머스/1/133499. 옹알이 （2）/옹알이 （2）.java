c56d7f1c4ef7357aaea3843cacf394fd7004569e
KimGemini <rlaalswo2437@gmail.com>
2026-06-10 23:23:21 +0900

    [level 1] Title: 옹알이 (2), Time: 3.95 ms, Memory: 81.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -0,0 +1,27 @@
+
+public class Solution {
+	public int solution(String[] babbling) {
+	
+		int cnt = 0;
+		
+		String[] baby = {"aya", "ye", "woo", "ma"};
+		
+		for(String word : babbling) {
+			
+			for(String b : baby) {
+				word = word.replace(b + b, "x");
+			}
+			
+			for(String b : baby) {
+				word = word.replace(b, " ");
+			}
+			
+			if(word.trim().isEmpty()) cnt++;
+			
+		}
+		
+		
+		return cnt;
+		
+	}
+}
