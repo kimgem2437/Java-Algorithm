ad0e3e17f85080c03c275e0041144b5d37319602
KimGemini <rlaalswo2437@gmail.com>
2026-07-17 09:26:51 +0900

    [level 1] Title: 햄버거 만들기, Time: 34.95 ms, Memory: 162 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -1,35 +1,23 @@
-import java.util.Stack;
-
-public class Solution {
-	public int solution(int[] ingredient) {
-
-		int cnt = 0;
-		Stack<Integer> st = new Stack<>();
-
-		
-		for(int i = 0; i < ingredient.length; i++) {
-			st.push(ingredient[i]);
-			
-			int size = st.size();
-			
-			if(size >= 4 && 
-					st.get(size - 4) == 1 
-					&& st.get(size - 3) == 2 
-					&& st.get(size - 2) == 3 
-					&& st.get(size - 1) == 1) {
-				
-				cnt++;
-				
-				for(int j = 0; j < 4; j++) {
-					st.pop();
-				}
-			}
-
-			
-		}
-		
-		
-		return cnt;
-	}
-
+class Solution {
+    public int solution(int[] ingredient) {
+        
+        StringBuilder sb = new StringBuilder();
+        int result = 0;
+        
+        for(int i = 0; i < ingredient.length; i++) {
+            
+            sb.append(ingredient[i]);
+            int len = sb.length();
+            
+            if(len >= 4 && sb.substring(len - 4).equals("1231")) {
+                sb.delete(len - 4, len);
+                result++;
+            }
+            
+        }
+        
+        
+        
+        return result;
+    }
 }
