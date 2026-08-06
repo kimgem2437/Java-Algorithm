294100cacfdd75c3d9c4da2112faf6f39e4cc68f
KimGemini <228603643+kimgem2437@users.noreply.github.com>
2026-08-06 11:11:34 +0900

    [level 1] Title: 실패율, Time: 7.34 ms, Memory: 92.9 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,34 +1,49 @@
-import java.util.Arrays;
+import java.util.*;
 
-public class Solution {
-	public int[] solution(int N, int[] stages) {
-		
-		int[] cnt = new int[N+2];
-		double[] fail = new double[N+1];
-		int[] result = new int[N];
-		
-		for(int i = 0; i < stages.length; i++) {
-			cnt[stages[i]]++;
-		}
-		
-		int num = stages.length;
-		
-		for(int i = 1; i < N+1; i++) {
-			fail[i] = (num == 0) ? 0 : (double) cnt[i] / num;
-			
-			num -= cnt[i];
-		}
-		
-		Integer[] order = new Integer[N];
-		for(int i = 0; i < N; i++) order[i] = i + 1;
-		
-		Arrays.sort(order, (a, b) -> {
-			if(fail[a] == fail[b]) return a - b;
-			return Double.compare(fail[b], fail[a]);
-		});
-		
-		for(int i = 0; i < N; i++) result[i] = order[i];
-		return result;
-		
-	}
+class Solution {
+    public int[] solution(int N, int[] stages) {
+        
+        int[] count = new int[N + 2];
+        
+        for(int stage : stages){
+            count[stage]++;
+        }
+        
+        double[] fail = new double[N + 1];
+        
+        int players = stages.length;
+        
+        for(int i = 1; i <= N; i++){
+            
+            if(players == 0) {
+                fail[i] = 0;
+            } else {
+                fail[i] = (double) count[i] / players;
+            }
+            
+            players -= count[i];
+        }
+        
+        Integer[] stageNums = new Integer[N];
+        
+        for(int i = 0; i < N; i++){
+            stageNums[i] = i + 1;
+        }
+        
+        Arrays.sort(stageNums, (a, b) -> {
+            if(fail[a] == fail[b]){
+                return a - b;
+            }
+            
+            return Double.compare(fail[b], fail[a]);
+        });
+        
+        int[] result = new int[N];
+        
+        for(int i = 0; i < N; i++){
+            result[i] = stageNums[i];
+        }        
+        
+        return result;
+    }
 }
