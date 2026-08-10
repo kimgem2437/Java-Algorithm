29788cacc5699b8d95ced8aec8036339f3af7b94
KimGemini <228603643+kimgem2437@users.noreply.github.com>
2026-08-10 11:44:48 +0900

    [level 2] Title: 소수 찾기, Time: 10.06 ms, Memory: 85.3 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -2,59 +2,62 @@ import java.util.*;
 
 class Solution {
     
-    Set<Integer> set = new HashSet<>();
     boolean[] visited;
+    Set<Integer> set = new HashSet<>();
     
     public int solution(String numbers) {
         
         visited = new boolean[numbers.length()];
         
-        makeNumber(numbers, "");
+        dfs(numbers, "");
         
-        int answer = 0;
+        int cnt = 0;
         
         for(int num : set) {
-            if(isPrime(num)){
-                answer++;
+            
+            if(Prime(num)) {
+                cnt++;
             }
-        }
-        
-        return answer;
-        
-    }
-    
-    public void makeNumber(String numbers, String current) {
-        
-        if(!current.equals("")) {
-         
-            set.add(Integer.parseInt(current));
             
         }
         
+        return cnt;
+        
+    }
+    
+    public void dfs(String numbers, String current){
+        
+        if(!current.equals("")){
+            set.add(Integer.parseInt(current));
+        }
+        
         for(int i = 0; i < numbers.length(); i++){
-            if(visited[i]){
+            
+            if(visited[i]) {
                 continue;
             }
             
             visited[i] = true;
-            makeNumber(numbers, current + numbers.charAt(i));
+            dfs(numbers, current + numbers.charAt(i));
             visited[i] = false;
+            
         }
+        
     }
     
-    public boolean isPrime(int num) {
+    public boolean Prime(int num){
         
-        if(num < 2) {
-            return false;
-        }
+        if(num < 2) return false;
         
         for(int i = 2; i * i <= num; i++){
-         
+            
             if(num % i == 0){
                 return false;
             }
+    
         }
         
         return true;
+        
     }
 }
