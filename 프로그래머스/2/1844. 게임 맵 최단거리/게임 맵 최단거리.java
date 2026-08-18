31cdae40aa985d6461956bba0ccab7b04357237e
KimGemini <228603643+kimgem2437@users.noreply.github.com>
2026-08-18 11:50:25 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.21 ms, Memory: 58.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -3,54 +3,55 @@ import java.util.*;
 class Solution {
     
     boolean[][] visited;
-    int[] dr = {0, 0, 1, -1};
+    int[] dr = {0, 0 , 1, -1};
     int[] dc = {1, -1, 0, 0};
-    int result = 0;
+    int answer = 0;
     
     public int solution(int[][] maps) {
         
-        int r = maps.length;
-        int c = maps[0].length;
+        visited = new boolean[maps.length][maps[0].length];
         
-        visited = new boolean[r][c];
         Queue<int[]> que = new LinkedList<>();
         
         que.offer(new int[]{0, 0, 1});
-        visited[0][0] = true;
         
         while(!que.isEmpty()) {
             
-            int[] current = que.poll();
+            int[] curr = que.poll();
+            visited[0][0] = true;
             
-            int currentR = current[0];
-            int currentC = current[1];
-            int distance = current[2];
+            int r = curr[0];
+            int c = curr[1];
+            int dis = curr[2];
             
-            if(currentR == r - 1 && currentC == c - 1){
-                return distance;
+            if(r == maps.length - 1 && c == maps[0].length -1) {
+                return dis;
             }
             
-            for(int i = 0; i < 4; i++){
+            for(int i = 0; i < 4; i++) {
                 
-                int nr = currentR + dr[i];
-                int nc = currentC + dc[i];
+                int nr = dr[i] + r;
+                int nc = dc[i] + c;
                 
-                if(nr < 0 || nr >= r || nc < 0 || nc >= c) {
+                if(nr >= maps.length || nr < 0 || nc >= maps[0].length || nc < 0) {
                     continue;
                 }
                 
-                if(maps[nr][nc] == 0) {
+                if(visited[nr][nc]) {
                     continue;
                 }
                 
-                if(visited[nr][nc]){
+                if (maps[nr][nc] == 0) {
                     continue;
                 }
                 
                 visited[nr][nc] = true;
-                que.offer(new int[]{nr, nc, distance + 1});
+                que.offer(new int[]{nr, nc, dis + 1});
+                
             }
+            
         }
+        
         
         return -1;
     }
