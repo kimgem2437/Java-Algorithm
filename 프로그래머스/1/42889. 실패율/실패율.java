f67f905f90f9365cd444105bc7434b8399606586
KimGemini <228603643+kimgem2437@users.noreply.github.com>
2026-08-18 11:15:48 +0900

    [level 1] Title: 실패율, Time: 10.75 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -3,46 +3,41 @@ import java.util.*;
 class Solution {
     public int[] solution(int N, int[] stages) {
         
-        int[] count = new int[N + 2];
+        int[] result = new int[N];
+        double[] fail = new double[N + 2];
+        int[] num = new int[N + 2];
+        Integer[] stageNum = new Integer[N];
         
-        for(int stage : stages){
-            count[stage]++;
+        for(int i = 0; i < stages.length; i++){
+            num[stages[i]]++;
         }
         
-        double[] fail = new double[N + 1];
+        int total = stages.length;
         
-        int players = stages.length;
-        
-        for(int i = 1; i <= N; i++){
-            
-            if(players == 0) {
+        for(int i = 1; i < fail.length; i++) {
+            if (total == 0) {
                 fail[i] = 0;
             } else {
-                fail[i] = (double) count[i] / players;
+                fail[i] = (double) num[i] / total;
             }
             
-            players -= count[i];
+            total -= num[i];
         }
         
-        Integer[] stageNums = new Integer[N];
-        
-        for(int i = 0; i < N; i++){
-            stageNums[i] = i + 1;
+        for (int i = 0; i < N; i++) {
+            stageNum[i] = i + 1;
         }
         
-        Arrays.sort(stageNums, (a, b) -> {
-            if(fail[a] == fail[b]){
+        Arrays.sort(stageNum, (a, b) -> {
+            if(fail[a] == fail[b]) {
                 return a - b;
             }
-            
             return Double.compare(fail[b], fail[a]);
         });
         
-        int[] result = new int[N];
-        
-        for(int i = 0; i < N; i++){
-            result[i] = stageNums[i];
-        }        
+        for (int i = 0; i < N; i++) {
+            result[i] = stageNum[i];
+        }
         
         return result;
     }
