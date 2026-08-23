8c66be7dbbdb30a35f31e6adc4e8250b1ea2eeb3
KimGemini <228603643+kimgem2437@users.noreply.github.com>
2026-08-23 10:51:06 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.28 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -3,54 +3,45 @@ class Solution {
         
         StringBuilder sb = new StringBuilder();
         
+        new_id = new_id.toLowerCase();
+        
         for(int i = 0; i < new_id.length(); i++){
             
             char c = new_id.charAt(i);
             
-            if(c >= 'A' && c <= 'Z'){
-                c = Character.toLowerCase(c);
+            if((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') || c == '-' || c == '_' || c == '.'){
+                if(c == '.' && sb.length() > 0 && sb.charAt(sb.length() - 1) == '.'){
+                    continue;
+                }
+                sb.append(c);
             }
             
-            boolean isLetter = c >= 'a' && c <= 'z';
-            boolean isNumber = c >= '0' && c <= '9';
-            boolean isSpecial = c == '-' || c == '_' || c == '.';
-            
-            if(!(isLetter || isNumber || isSpecial)){
-                continue;
-            }
-            
-            if(c == '.' && sb.length() > 0 && sb.charAt(sb.length() - 1) == '.'){
-                continue;
-            }
-            
-            sb.append(c);
         }
-            
-        if(sb.length() > 0 && sb.charAt(0) == '.'){
+        
+        if(sb.length() > 0 && sb.charAt(0) == '.') {
             sb.deleteCharAt(0);
         }
         
-        if(sb.length() > 0 && sb.charAt(sb.length() -1) == '.'){
-            sb.deleteCharAt(sb.length() - 1); 
+        if(sb.length() > 0 && sb.charAt(sb.length() - 1) == '.') {
+            sb.deleteCharAt(sb.length() - 1);
+        }
+        
+        if(sb.length() >= 16){
+            sb = new StringBuilder(sb.substring(0, 15));
+        
+                if(sb.length() > 0 && sb.charAt(sb.length() - 1) == '.') {
+                sb.deleteCharAt(sb.length() - 1);
+            }
         }
         
         if(sb.length() == 0) {
             sb.append("a");
         }
         
-        if(sb.length() >= 16) {
-            sb.setLength(15);
-            
-            if(sb.charAt(sb.length() - 1) == '.') {
-                sb.deleteCharAt(sb.length() - 1);
-            }
-        }
-        
-        while(sb.length() < 3){
+        while(sb.length() < 3) {
             sb.append(sb.charAt(sb.length() - 1));
         }
-        
+    
         return sb.toString();
-        
     }
 }
