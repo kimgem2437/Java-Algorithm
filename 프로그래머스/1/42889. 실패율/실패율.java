30127687011d0e324f091a41072d247d4132a253
KimGemini <228603643+kimgem2437@users.noreply.github.com>
2026-08-30 13:34:01 +0900

    [level 1] Title: 실패율, Time: 5.84 ms, Memory: 96.8 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -4,23 +4,22 @@ class Solution {
     public int[] solution(int N, int[] stages) {
         
         double[] fail = new double[N + 2];
-        int[] count = new int[N + 2]; 
-        int people = stages.length;
-                
+        int[] cnt = new int[N + 2];
+        
         for(int i = 0; i < stages.length; i++){
-            count[stages[i]]++;
+            cnt[stages[i]]++;
         }
         
-        int total = people;
+        int total = stages.length;
         
         for(int i = 1; i <= N; i++){
-            if(total == 0) {
+            
+            if(total == 0){
                 fail[i] = 0;
             } else {
-                fail[i] = (double) count[i] / total;
+                fail[i] = (double) cnt[i] / total;
             }
-            
-            total -= count[i];
+            total -= cnt[i];
         }
         
         Integer[] stageNum = new Integer[N];
@@ -30,7 +29,7 @@ class Solution {
         }
         
         Arrays.sort(stageNum, (a, b) -> {
-            if(fail[a] == fail[b]) {
+            if(fail[a] == fail[b]){
                 return a - b;
             }
             return Double.compare(fail[b], fail[a]);
