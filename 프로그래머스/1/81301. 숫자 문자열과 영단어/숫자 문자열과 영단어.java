b2d9ddbdec59ba25e8504cc32b0b99c5403b399f
KimGemini <228603643+kimgem2437@users.noreply.github.com>
2026-09-01 16:02:24 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.06 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,50 +1,20 @@
 class Solution {
     public int solution(String s) {
         
-        String[] eng = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        String[] st = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
         
-        StringBuilder sb = new StringBuilder();
+        s = s.replace("zero", st[0]);
+        s = s.replace("one", st[1]);
+        s = s.replace("two", st[2]);
+        s = s.replace("three", st[3]);
+        s = s.replace("four", st[4]);
+        s = s.replace("five", st[5]);
+        s = s.replace("six", st[6]);
+        s = s.replace("seven", st[7]);
+        s = s.replace("eight", st[8]);
+        s = s.replace("nine", st[9]);
         
-        for(int i = 0; i < s.length(); i++){
-            
-            char c = s.charAt(i);
-            
-            if(c >= '0' && c <= '9'){
-                sb.append(c);
-            } else if(c == 'z'){
-                sb.append("0");
-                i += 3;
-            } else if(c == 'o'){
-                sb.append("1");
-                i += 2;
-            } else if(c == 'e'){
-                sb.append("8");
-                i += 4;
-            } else if(c == 'n'){
-                sb.append("9");
-                i += 3;
-            } else if(i + 1 < s.length() && c == 't' && s.charAt(i + 1) == 'w'){
-                sb.append("2");
-                i += 2;
-            } else if(i + 1 < s.length() && c == 't' && s.charAt(i + 1) == 'h'){
-                sb.append("3");
-                i += 4;
-            } else if(i + 1 < s.length() && c == 'f' && s.charAt(i + 1) == 'o'){
-                sb.append("4");
-                i += 3;
-            } else if(i + 1 < s.length() && c == 'f' && s.charAt(i + 1) == 'i'){
-                sb.append("5");
-                i += 3;
-            } else if(i + 1 < s.length() && c == 's' && s.charAt(i + 1) == 'i'){
-                sb.append("6");
-                i += 2;
-            } else if(i + 1 < s.length() && c == 's' && s.charAt(i + 1) == 'e') {
-                sb.append("7");
-                i += 4;
-            }
-        }
-        
-        int result = Integer.parseInt(sb.toString());
+        int result = Integer.parseInt(s);
         
         return result;
     }
