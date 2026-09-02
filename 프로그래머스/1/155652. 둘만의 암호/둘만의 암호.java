3f75e7eb33e04f1ec4deccd72d33f667a6535147
KimGemini <228603643+kimgem2437@users.noreply.github.com>
2026-09-02 17:29:51 +0900

    [level 1] Title: 둘만의 암호, Time: 1.08 ms, Memory: 86.9 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -1,33 +1,28 @@
-import java.util.*;
-
 class Solution {
     public String solution(String s, String skip, int index) {
         
-        String result = "";
+        StringBuilder sb = new StringBuilder();
+        
         
         for(int i = 0; i < s.length(); i++){
             
             char c = s.charAt(i);
-            int cnt = 0;
             
+            int cnt = 0;
             while(cnt < index){
                 c++;
-                
-                if(c > 'z'){
-                    c = 'a';
+                if(c > 'z') {
+                    c -= 26;
                 }
-                
                 if(skip.contains(String.valueOf(c))){
                     continue;
+                } else {
+                    cnt++;
                 }
-                
-                cnt++;
             }
-            
-            result += c;
+            sb.append(c);
         }
         
-        
-        return result;
+        return sb.toString();
     }
 }
