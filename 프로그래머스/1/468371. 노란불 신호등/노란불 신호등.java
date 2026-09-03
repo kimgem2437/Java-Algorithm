403e29bf3d7eb236b71130fc814950b82d628e4a
KimGemini <228603643+kimgem2437@users.noreply.github.com>
2026-09-03 17:22:07 +0900

    [level 1] Title: 노란불 신호등, Time: 8.06 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.java
+++ 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.java
@@ -3,49 +3,47 @@ class Solution {
         
         int limit = 1;
         
-        for(int i = 0; i < signals.length; i++) {
+        for(int i = 0; i < signals.length; i++){
             int cycle = signals[i][0] + signals[i][1] + signals[i][2];
             limit = lcm(limit, cycle);
         }
         
-        for(int t = 1; t <= limit; t++) {
+        for(int t = 1; t <= limit; t++){
             
-            boolean yellow = true;
+            boolean allYellow = true;
             
-            for(int i = 0; i < signals.length; i++) {
+            for(int i = 0; i < signals.length; i++){
+                int g = signals[i][0];
+                int y = signals[i][1];
+                int r = signals[i][2];
                 
-                int G = signals[i][0];
-                int Y = signals[i][1];
-                int R = signals[i][2];
+                int cycle = g + y + r;
+                int pos = (t - 1) % cycle;
                 
-                int cycle = G + Y + R;
-                int now = (t - 1) % cycle + 1;
-                
-                if(!(now > G && now <= G + Y)){
-                    yellow = false;
+                if(!(pos >= g && pos < g + y)){
+                    allYellow = false;
                     break;
                 }
             }
             
-            if(yellow) {
+            if(allYellow){
                 return t;
-            }
+            }  
         }
         
         return -1;
-        
+    }
+    
+    public int gcd(int a, int b){
+        while(b != 0){
+            int temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
     }
     
     public int lcm(int a, int b){
-        
-        int num = Math.max(a, b);
-        
-        while(true){
-            if(num % a == 0 && num % b == 0) {
-                return num;
-            }
-            num++;
-        }
-        
+        return a / gcd(a, b) * b;
     }
 }
