3c834f0d5d304b060ed50250dbf0eab14afacbb0
KimGemini <228603643+kimgem2437@users.noreply.github.com>
2026-09-08 16:13:20 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.70 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -1,31 +1,27 @@
 import java.util.*;
-    
+
 class Solution {
     public int solution(String str1, String str2) {
         
         Map<String, Integer> map1 = makeMap(str1);
         Map<String, Integer> map2 = makeMap(str2);
         
+        Set<String> keys = new HashSet<>();
+        keys.addAll(map1.keySet());
+        keys.addAll(map2.keySet());
+        
         int intersection = 0;
         int union = 0;
         
-        for(String key : map1.keySet()) {
+        for (String key : keys) {
+            int count1 = map1.getOrDefault(key, 0);
+            int count2 = map2.getOrDefault(key, 0);
             
-            int cnt1 = map1.get(key);
-            int cnt2 = map2.getOrDefault(key, 0);
-            
-            intersection += Math.min(cnt1, cnt2);
-            union += Math.max(cnt1, cnt2);
+            intersection += Math.min(count1, count2);
+            union += Math.max(count1, count2);
         }
         
-        for(String key : map2.keySet()){
-            if(!map1.containsKey(key)) {
-                union += map2.get(key);
-            }
-        }
-        
-        
-        if(union == 0) {
+        if (union == 0) {
             return 65536;
         }
         
@@ -35,23 +31,18 @@ class Solution {
     public Map<String, Integer> makeMap(String str) {
         
         Map<String, Integer> map = new HashMap<>();
-        
         str = str.toUpperCase();
         
-        for(int i = 0; i < str.length() - 1; i++){
+        for (int i = 0; i < str.length() - 1; i++) {
+            String word = str.substring(i, i + 2);
             
-            char first = str.charAt(i);
-            char second = str.charAt(i + 1);
+            char a = word.charAt(0);
+            char b = word.charAt(1);
             
-            if(first >= 'A' && first <= 'Z' && second >= 'A' && second <= 'Z') {
-                
-                String word = str.substring(i, i + 2);
-                
+            if (a >= 'A' && a <= 'Z' && b >= 'A' && b <= 'Z') {
                 map.put(word, map.getOrDefault(word, 0) + 1);
             }
-            
         }
-        
         
         return map;
     }
