9f5cdda2f933aba62f6101c247bc54214346306e
KimGemini <228603643+kimgem2437@users.noreply.github.com>
2026-09-09 15:42:11 +0900

    [level 2] Title: 짝지어 제거하기, Time: 51.40 ms, Memory: 66.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution
+{
+    public int solution(String s)
+    {
+        
+        Stack<Character> stack = new Stack<>();
+        
+        for(int i = 0; i < s.length(); i++){
+            
+            char c = s.charAt(i);
+            
+            if(!stack.isEmpty() && stack.peek() == c){
+                stack.pop();
+            } else {
+                stack.add(c);
+            }
+        }
+        
+        if(stack.isEmpty()){
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
