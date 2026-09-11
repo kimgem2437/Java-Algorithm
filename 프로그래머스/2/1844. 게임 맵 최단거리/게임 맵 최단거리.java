4fa7af6cc61cb2467722579a0430ec17177ef645
KimGemini <228603643+kimgem2437@users.noreply.github.com>
2026-09-12 00:17:29 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.39 ms, Memory: 57.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,47 +2,54 @@ import java.util.*;
 
 class Solution {
     
-    int[] dr = {0, 0, -1, 1};
-    int[] dc = {-1 , 1 ,0 ,0};
     Queue<int[]> que = new LinkedList<>();
     boolean[][] visited;
+    int[] dr = {0, 0, -1, 1};
+    int[] dc = {1, -1, 0, 0};
     
     public int solution(int[][] maps) {
         
-        int n = maps.length;
-        int m = maps[0].length;
+        int result = 0;
+        visited = new boolean[maps.length][maps[0].length];
         
-        visited = new boolean[n][m];
+        int finishR = maps.length-1;
+        int finishC = maps[0].length-1;
         
         que.offer(new int[]{0, 0, 1});
         
-         while(!que.isEmpty()){
+        while(!que.isEmpty()){
             
-            int[] location = que.poll();
-            int r = location[0];
-            int c = location[1];
-            int distance = location[2];
-             
-            if(r == n - 1 && c == m - 1) {
-                return distance;
+            int[] map = que.poll();
+            
+            int r = map[0];
+            int c = map[1];
+            int dis = map[2];
+            
+            
+            if(r == finishR && c == finishC){
+                result = dis;
+                break;
             }
-             
+            
             for(int i = 0; i < 4; i++){
-            
-                int br = r + dr[i];
-                int bc = c + dc[i];
-
-                if(br >= 0 && br < maps.length && bc >= 0 && bc < maps[br].length 
-                   && maps[br][bc] == 1 && !visited[br][bc]){
-                    
-                    visited[br][bc] = true;
-                    que.offer(new int[]{br, bc, distance + 1});
+                
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+                
+                if(nr >= 0 && nr < maps.length && nc >= 0 && nc < maps[nr].length
+                  && !visited[nr][nc] && maps[nr][nc] == 1){
+                    visited[nr][nc] = true;
+                    que.offer(new int[]{nr, nc, dis + 1});
                 }
+                
             }
-            
-             
-         }
+        }
         
-        return -1;
+        if(visited[finishR][finishC]){
+            return result;
+        } else {
+            return -1;
+        }
+
     }
 }
