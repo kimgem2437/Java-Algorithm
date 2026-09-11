5cbf0705cff69bcf6e4bd55aa96a550f4c610572
KimGemini <228603643+kimgem2437@users.noreply.github.com>
2026-09-12 00:47:34 +0900

    [level 2] Title: 호텔 대실, Time: 4.29 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -1,43 +1,45 @@
 import java.util.*;
 
 class Solution {
+
     public int solution(String[][] book_time) {
-        
+
         int[][] times = new int[book_time.length][2];
-        
-        for(int i = 0; i < book_time.length; i++){
+
+        for (int i = 0; i < book_time.length; i++) {
             times[i][0] = toMinute(book_time[i][0]);
             times[i][1] = toMinute(book_time[i][1]) + 10;
         }
-        
+
+        // 시작 시간 기준 정렬
         Arrays.sort(times, (a, b) -> a[0] - b[0]);
-        
+
+        // 각 방이 언제 사용 가능해지는지 저장
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        
-        for(int i = 0; i < times.length; i++){
-            
-            int start = times[i][0];
-            int end = times[i][1];
-            
-            if(!pq.isEmpty() && pq.peek() <= start){
+
+        for (int[] time : times) {
+
+            int start = time[0];
+            int end = time[1];
+
+            // 가장 빨리 비는 방을 재사용 가능
+            if (!pq.isEmpty() && pq.peek() <= start) {
                 pq.poll();
             }
-            
+
             pq.offer(end);
         }
-        
+
         return pq.size();
     }
-    
-    public int toMinute(String time) {
-        
-        String[] st = time.split(":");
-        
-        int hour = Integer.parseInt(st[0]);
-        int minute = Integer.parseInt(st[1]);
-        
+
+    private int toMinute(String time) {
+
+        String[] arr = time.split(":");
+
+        int hour = Integer.parseInt(arr[0]);
+        int minute = Integer.parseInt(arr[1]);
+
         return hour * 60 + minute;
-        
-        
     }
 }
